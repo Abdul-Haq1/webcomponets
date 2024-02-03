67f43b48e1b012587b589863571c3438864d8055
AbdulHaq <abdulhaqhk@gmail.com>
2024-02-03 18:02:27 +0530

    complete visual part

---
+++ carsoul/js/main.js
@@ -0,0 +1,6 @@
+fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data.drinks[0])
+        document.getElementById('carouselImg').src = data.drinks[0].strDrinkThumb
+    })
