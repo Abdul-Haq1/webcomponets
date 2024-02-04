b616de4379c17efdd103da9ca87e9b4ad412f8a4
Abdul Haq <abdulhaqhk@gmail.com>
2024-02-04 19:36:44 +0530

    carsoul is commpleted

--- carsoul/js/main.js
+++ carsoul/js/main.js
@@ -1,6 +1,67 @@
+const images = []
+
 fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
     .then(res => res.json())
     .then(data =>{
         console.log(data.drinks[0])
         document.getElementById('carouselImg').src = data.drinks[0].strDrinkThumb
     })
+
+fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(data =>{
+        images.push(data.drinks[0].strDrinkThumb)
+    })
+    
+fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(data =>{
+        images.push(data.drinks[0].strDrinkThumb)
+    }) 
+    
+fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(data =>{
+        images.push(data.drinks[0].strDrinkThumb)
+    })
+fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(data =>{
+        images.push(data.drinks[0].strDrinkThumb)
+    })
+    
+fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(data =>{
+        images.push(data.drinks[0].strDrinkThumb)
+    })
+
+console.log(images)
+
+let currentIndex = 0;
+let autoSlideInterval;
+
+function updateImage(){
+    const imgElement = document.getElementById('carouselImg');
+    imgElement.src = images[currentIndex];
+}
+
+function nextImg() {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateImage();
+}
+
+function previousImg(){
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateImage();
+}
+
+function scheduleAutoSlide(){
+    autoSlideInterval = setInterval(() => {
+        nextImg();
+    }, 3000)
+}
+
+// Initial rendering
+updateImage();
+scheduleAutoSlide();
