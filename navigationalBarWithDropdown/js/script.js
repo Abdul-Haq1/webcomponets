b9ba48207b700467e2e1b8aa64586b92b4dda4a0
Abdul Haq <abdulhaqhk@gmail.com>
2024-03-20 21:17:02 +0530

    navigationalWith further dropdopdown completed.

--- navigationalBarWithDropdown/js/script.js
+++ navigationalBarWithDropdown/js/script.js
@@ -5,6 +5,18 @@ const dropdown = document.querySelectorAll(".dropdown");
 const links = document.querySelectorAll(".dropdown a");
 
 
+
+function setAriaExpandedFalse() {
+    dropdownBtn.forEach((btn) => btn.setAttribute("aria-expanded", "false"));
+}
+
+function closeDropdownMenu() {
+    dropdown.forEach((drop) => {
+        drop.classList.remove("active");
+        drop.addEventListener("click", (e) => e.stopPropagation());
+    });
+}
+
 function toggleHamburger() {
     navMenu.classList.toggle("show");
     hamburgerBtn.setAttribute(
@@ -12,6 +24,45 @@ function toggleHamburger() {
         hamburgerBtn.getAttribute("aria-expanded") === "false" ? "true" : "false"
     );
 }
+
+dropdownBtn.forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+        const dropdownIndex = e.currentTarget.dataset.dropdown;
+        const dropdownElement = document.getElementById(dropdownIndex);
+
+        dropdownElement.classList.toggle("active");
+        dropdown.forEach((drop) => {
+            if (drop.id != btn.dataset["dropdown"]) {
+                drop.classList.remove("active");
+            }
+        });
+        e.stopPropagation();
+    });
+});
+
+// close dropdown menu when the dropdown links are clicked
+
+links.forEach((link) => {
+    link.addEventListener('click', () => {
+        closeDropdownMenu()
+        setAriaExpandedFalse()
+        toggleHamburger()
+    })
+})
+
+// close dropdown menu when you click on the document body
+document.documentElement.addEventListener("click", () => {
+    closeDropdownMenu();
+    setAriaExpandedFalse();
+});
+
+// close dropdown when the escape key is pressed
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+        closeDropdownMenu();
+        setAriaExpandedFalse();
+    }
+});
 
 
 
